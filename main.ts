3b2d94ba0aee0140c5790acb484d8bd247f5e5cd
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-04-14 12:10:20 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Exceso de ruido hoy")
+    basic.showNumber(Ruido)
+    basic.showString("veces")
+})
+input.onSound(DetectedSound.Loud, function () {
+    for (let index = 0; index < 4; index++) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(100)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    }
+})
+let Ruido = 0
+Ruido = 0
+basic.showString("“Iniciando sistema de control de sonido y temperatura”")
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
+basic.forever(function () {
+	
+})
