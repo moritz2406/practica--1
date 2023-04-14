9f55ca6fd163a6c131c576602adf26cc0420a9e5
moritz2406 <122538931+moritz2406@users.noreply.github.com>
2023-04-14 12:19:40 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,9 @@ input.onSound(DetectedSound.Loud, function () {
     for (let index = 0; index < 4; index++) {
         pins.digitalWritePin(DigitalPin.P0, 1)
         basic.pause(100)
-        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P0, 0)
     }
+    Ruido = 1
 })
 let Ruido = 0
 Ruido = 0
@@ -21,5 +22,20 @@ basic.showLeds(`
     . # . . .
     `)
 basic.forever(function () {
-	
+    while (input.temperature() == 0) {
+        for (let index = 0; index < 5; index++) {
+            basic.showLeds(`
+                . . # # #
+                # . # . .
+                . . # . .
+                . . # . .
+                . . # # #
+                `)
+            basic.pause(100)
+            basic.showArrow(ArrowNames.South)
+        }
+    }
+    basic.clearScreen()
+    pins.analogWritePin(AnalogPin.P10, 27)
+    basic.pause(100)
 })
